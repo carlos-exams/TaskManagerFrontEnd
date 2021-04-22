dd7d54dd7d7b97ab5e341d41ea698fb54b2a8e6e
Carlos Cortina <carlos.cortina@mimacom.com>
2021-04-22 17:21:38 +0200

    Adding Task Status

--- src/app/models/task.ts
+++ src/app/models/task.ts
@@ -2,6 +2,7 @@ export class Task {
     id: number;
     name: string;
     description: string;
-    creation_date: Date;
-    update_date: Date;
+    creationDate: Date;
+    updateDate: Date;
+    statusName: string;
 }
