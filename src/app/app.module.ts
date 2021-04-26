d1c1e38b6935530d4db7a2205d1f8cfc5a94148e
Carlos Cortina <carlos.cortina@mimacom.com>
2021-04-26 12:00:23 +0200

    Headers using PrimeNg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './commons/header/header.component';
 import { TaskModule } from './views/task/task.module';
 import { HttpClientModule } from '@angular/common/http';
-import { TableModule } from 'primeng/table';
-import { ToolbarModule} from 'primeng/toolbar';
-import { ButtonModule } from 'primeng/button';
+import {MenubarModule} from 'primeng/menubar';
+
 
 @NgModule({
   declarations: [
@@ -18,9 +17,8 @@ import { ButtonModule } from 'primeng/button';
     BrowserModule,
     TaskModule,
     HttpClientModule,
-    TableModule,
-    ToolbarModule,
-    ButtonModule
+    MenubarModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/commons/header/header.component.ts
+++ src/app/commons/header/header.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import {MenuItem} from 'primeng/api';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  items: MenuItem[];
 
   constructor() { }
 
   ngOnInit() {
-    console.log()
+    this.items = [
+      {
+          label: 'Task Manager',
+      }
+  ];
   }
 
 }
